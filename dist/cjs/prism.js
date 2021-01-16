e5babbb1e6473834fe64c3074faa40a51156499a
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-01-17 05:11:47 +0800

    Update prism.js

--- dist/cjs/prism.js
+++ dist/cjs/prism.js
@@ -9,8 +9,13 @@ exports.default = void 0;
 
 var _highlight = _interopRequireDefault(require("./highlight"));
 
-var _core = _interopRequireDefault(require("refractor/core"));
+var _prism = _interopRequireDefault(require("./styles/prism/prism"));
 
-var highlighter = (0, _highlight.default)(_core.default, {});
+var _refractor = _interopRequireDefault(require("refractor"));
+
+var _supportedLanguages = _interopRequireDefault(require("./languages/prism/supported-languages"));
+
+var highlighter = (0, _highlight.default)(_refractor.default, _prism.default);
+highlighter.supportedLanguages = _supportedLanguages.default;
 var _default = highlighter;
 exports.default = _default;
